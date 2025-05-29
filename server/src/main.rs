c6b08f0afd569d71a9c28d6e8a0fe395486e7be4
wesleygoyette <wesley.goyette@gmail.com>
2025-05-28 23:34:08 -0500

    yeat

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let tcp_addr = format!("0.0.0.0:{}", TCP_PORT);
-    let udp_addr = format!("0.0.0.0:{}", UDP_PORT);
+    let udp_addr = format!("fly-global-services:{}", UDP_PORT);
 
     let server = WeSFU::new(tcp_addr, udp_addr).await?;
 
